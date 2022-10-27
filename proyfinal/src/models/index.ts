5166fe23014ba8d40ea5610c1004f1f897127745
hpinzon9 <hpinzon9@gmail.com>
2022-10-26 19:25:26 -0500

    correccion de proyecto

--- proyfinal/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './empresa.model';
-export * from './empleado.model';
-export * from './directivo.model';
-export * from './cliente.model';
-export * from './servicio.model';
--- proyfinal/src/repositories/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './cliente.repository';
-export * from './directivo.repository';
-export * from './empleado.repository';
-export * from './empresa.repository';
-export * from './servicio.repository';
