45ffbc432d97c2b0e12cd6c96781b3284dbea6f1
hpinzon9 <hpinzon9@gmail.com>
2022-11-01 16:21:42 -0500

    actualizando proyecto

--- proyservicio/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './empresa.model';
-export * from './directivo.model';
-export * from './empleado.model';
-export * from './cliente.model';
-export * from './servicio.model';
-export * from './notificaciones.model';
